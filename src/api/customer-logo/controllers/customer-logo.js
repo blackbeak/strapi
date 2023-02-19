5d357fb8eb6abdae4a07766b434545ee8e462d44
Steve Jackson <stevejackson@Steves-MacBook-Pro-3.local>
2023-02-19 13:38:58 +0200

    First strapi update

---
+++ src/api/customer-logo/controllers/customer-logo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  customer-logo controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::customer-logo.customer-logo');
---
+++ src/api/customer-logo/routes/customer-logo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * customer-logo router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::customer-logo.customer-logo');
