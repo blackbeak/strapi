93f03abe51dc43f1fb1d7d66dd8f0c658ae4e659
Steve Jackson <stevejackson@Steves-MacBook-Pro-3.local>
2025-02-09 14:54:17 +0200

    Added public url to server config for API

--- config/server.js
+++ config/server.js
@@ -1,6 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://strapi-production-74db.up.railway.app/'), // ✅ Add this line
+  
   app: {
     keys: env.array("APP_KEYS"),
   },
