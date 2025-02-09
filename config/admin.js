ea505d8d4bb9341171db3c3d0cea10b8b1793b44
Steve Jackson <stevejackson@Steves-MacBook-Pro-3.local>
2025-02-09 13:52:15 +0200

    Added server variables

--- config/admin.js
+++ config/admin.js
@@ -3,6 +3,7 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET', 'e4350524b0689fc3f46e2407dae950e2'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT', 'a1b2c3d4e5f6g7h8i9j0'),
+    salt: env('API_TOKEN_SALT'),
   },
+
 });
